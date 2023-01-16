e330f8a97770026486292bce2fcc6c1f2b168ce5
Dillip Singh <dk381759@gmail.com>
2023-01-16 23:29:55 +0545

    static files are corrected

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'stati_cdn')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
