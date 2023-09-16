89985825607b471845474b80ed2cddbea2d92136
Dillip Singh <dk381759@gmail.com>
2023-09-16 07:59:22 +0545

    Build_files edited

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-51f!z37f%ch%*v-_jghz)b3&ojk#zkeb3%63252-m8!(bv8vkt
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
