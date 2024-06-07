da6f721ab74ca3b3117bff1efaae17df27361d22
Dillip Singh <90038385+dillipsingh99@users.noreply.github.com>
2024-06-07 22:44:13 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # echo "BUILD START"
-pip install --root-user-action=ignore
-pip install -r requirements.txt
+pip3 install --root-user-action=ignore
+pip3 install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 # echo "BUILD END"
